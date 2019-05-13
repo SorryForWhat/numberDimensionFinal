c0bb4090749bcc0297eb3731a0ddb72d2ef4691d
Georgix <KarelinOlegSPB@yandex.ru>
2019-05-13 05:21:42 +0300

    fin fixes

--- src/main/java/Quantity.kt
+++ src/main/java/Quantity.kt
@@ -1,4 +1,5 @@
 package src
+
 /**
  * Possible Signs:
  * "/" - деление
@@ -6,45 +7,52 @@ package src
  * "+" - сложение
  * "-" - вычитание
  * "&" - сравнение
-*/
+ */
 
 
 
-data class Quantity(val x: Double, val u: String) { //Операции с плавающей точкой
-
-    fun uSum(q: Quantity) = if (u == q.u) Quantity(x + q.x, u) else throw Exception("Unit Mismatch") // Суммирование
-    fun uMin(q: Quantity) = if (u == q.u) Quantity(x - q.x, u) else throw Exception("Unit Mismatch") // Вычитание
-    fun uMul(q: Quantity) = if (u == q.u) Quantity(x * q.x, "$u^2") else throw Exception("Unit Mismatch") // Умножение
-    fun uDiv(q: Quantity) = if (u == q.u) x / q.x else throw Exception("Unit Mismatch") // Деление двух величин
-    fun rMul(r: Double) = Quantity(x * r, u) // Деление на число
-    fun rDiv(r: Double) = Quantity(x / r, u) // Умножение на число
-    fun convertToString():String { return "$x $u"} //Преобразование в строку
-
-
-}
-    fun readQuantity (s: String): Quantity {
-        val res = s.split(" ")
-        return Quantity(res[0].toDouble(), res[1])
-    }
-
-    fun simpleParser(unit1: Quantity, unit2: String, operation: String): String {
-
-            if (operation == "/") return unit1.rDiv(unit2.toDouble()).convertToString()
-            if (operation == "*") return unit1.rMul(unit2.toDouble()).convertToString()
-        else throw Exception("Invalid Expression Syntax")
+data class Quantity(val x: Double, val u: String)  { //Операции с плавающей точкой
+    fun convertToString(): String = "$x $u"
+    operator fun plus(q: Quantity) = if (u == q.u) Quantity(x + q.x, u).convertToString() else throw Exception("Unit Mismatch") // Суммирование
+    operator fun minus(q: Quantity) = if (u == q.u) Quantity(x - q.x, u).convertToString() else throw Exception("Unit Mismatch") // Вычитание
+    operator fun times(q: Quantity) = Quantity(x * q.x, "$u*${q.u}") // Умножение
+    operator fun div(q: Quantity) = Quantity(x / q.x, "$u/${q.u}") //Деление двух величин
+    operator fun times(r: Double) = Quantity(x * r, u).convertToString() // Деление на число
+    operator fun div(r: Double) = Quantity(x / r, u).convertToString() // Умножение на число
+    companion object {fun readQuantity(s: String): Quantity {
+            val res = s.split(" ")
+            if (res.size != 2) throw Exception("Invalid Quantity Syntax")
+            return Quantity(res[0].toDouble(), res[1])
         }
-
-    fun multiParser(unit1: Quantity, unit2: Quantity, operation: String): String {
-        if (operation == "/") return unit1.uDiv(unit2).toString()
-        if (operation == "+") return unit1.uSum(unit2).convertToString()
-        if (operation == "-") return unit1.uMin(unit2).convertToString()
-        if (operation == "&") if (unit1.u == unit2.u) {
-            val comparedUnit1 = unit1.convertToString()
-            val comparedUnit2 = unit2.convertToString()
-            if (unit1.x > unit2.x) return "$comparedUnit1 > $comparedUnit2"
-            if (unit1.x == unit2.x) return "$comparedUnit1 = $comparedUnit2"
-            if (unit1.x < unit2.x) return "$comparedUnit1 < $comparedUnit2"
-        } else throw Exception("Invalid Expression Syntax")
-        if (operation == "*") return unit1.uMul(unit2).convertToString()
-        else throw Exception("Invalid Expression Syntax")
+        }
     }
+
+
+    fun simpleParser(unit1: Quantity, unit2: Double, operation: String): String {
+        if (operation == "/") return unit1/unit2
+        if (operation == "*") return unit1*unit2
+    else throw Exception("Invalid Expression Syntax")
+}
+
+fun multiParser(unit1: Quantity, unit2: Quantity, operation: String): String {
+    if (operation == "/") {
+        return if (unit1.u == unit2.u) unit1.div(unit2).x.toString()
+        else unit1.div(unit2).convertToString()
+    }
+    if (operation == "*") {
+        return if (unit1.u == unit2.u) unit1.times(unit2).x.toString()+" ${unit1.u}^2"
+        else unit1.times(unit2).convertToString()
+    }
+    if (operation == "+") return unit1.plus(unit2)
+    if (operation == "-") return unit1.minus(unit2)
+    if (operation == "&") if (unit1.u == unit2.u) {
+
+        val comparedUnit1 = unit1.convertToString()
+        val comparedUnit2 = unit2.convertToString()
+        if (unit1.x > unit2.x) return "$comparedUnit1 > $comparedUnit2"
+        if (unit1.x == unit2.x) return "$comparedUnit1 = $comparedUnit2"
+        if (unit1.x < unit2.x) return "$comparedUnit1 < $comparedUnit2"
+    } else throw Exception("Invalid Expression Syntax")
+    else throw Exception("Invalid Expression Syntax")
+    throw Exception("Invalid Expression Syntax")
+}
--- src/main/java/test.kt
+++ src/main/java/test.kt
@@ -1,28 +1,41 @@
 package src
 
+
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.jupiter.api.assertThrows
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+
 
 class Tests {
+    val num1 = Quantity.readQuantity("12.2 метр")
+    val num2 = Quantity.readQuantity("5 секунд")
+    val num3 = Quantity.readQuantity("5.0 метр")
+    val num4 = Quantity.readQuantity("6.7 метр")
+    val num5 = Quantity.readQuantity("5.3 метр")
+    val num6 = Quantity.readQuantity("12 секунд")
+    val num7 = Quantity.readQuantity("12.5 метр")
+
     @Test
     fun readQuantity(){
-        assertEquals(Quantity(12.0, "секунд"), readQuantity ("12 секунд"))
-        assertEquals(Quantity(12.4, "метр"), readQuantity ("12.4 метр"))
+        assertEquals(Quantity(12.0, "секунд"), num6)
+        assertEquals(Quantity(12.5, "метр"), num7)
     }
     @Test
     fun multiParser() {
-        assertEquals("2.44" , multiParser(readQuantity("12.2 метр"), readQuantity("5.0 метр"), "/"))
-        assertEquals("25.0 метр^2" , multiParser(readQuantity("5.0 метр"), readQuantity("5.0 метр"), "*"))
-        assertEquals("12.0 метр" , multiParser(readQuantity("5.3 метр"), readQuantity("6.7 метр"), "+"))
-        assertEquals("13.5 метр" , multiParser(readQuantity("19.0 метр"), readQuantity("5.5 метр"), "-"))
-        assertEquals("5.0 метр = 5.0 метр" , multiParser(readQuantity("5.0 метр"), readQuantity("5.0 метр"), "&"))
-        assertEquals("5.0 метр < 5.1 метр" , multiParser(readQuantity("5.0 метр"), readQuantity("5.1 метр"), "&"))
-        assertEquals("5.1 метр > 4.9 метр" , multiParser(readQuantity("5.1 метр"), readQuantity("4.9 метр"), "&"))
+        assertEquals("2.44 метр/секунд" , multiParser(num1 , num2, "/"))
+        assertEquals("2.44" , multiParser(num1, num3, "/"))
+        assertEquals("25.0 метр^2" , multiParser(num3, num3, "*"))
+        assertEquals("12.0 метр" , multiParser(num4, num5, "+"))
+        assertEquals("7.5 метр" , multiParser(num7, num3, "-"))
+        assertEquals("12.2 метр > 5.0 метр" , multiParser(num1, num3, "&"))
+        assertEquals("5.0 метр = 5.0 метр" , multiParser(num3, num3, "&"))
     }
     @Test
     fun simpleParser() {
-        assertEquals("1.0 метр" , simpleParser(readQuantity("5.0 метр"), "5.0", "/"))
-        assertEquals("24.6 метр" , simpleParser(readQuantity("12.3 метр"), "2.0", "*"))
+        assertEquals("1.0 метр" , simpleParser(num3, 5.0, "/"))
+        assertEquals("24.4 метр" , simpleParser(num1, 2.0, "*"))
     }
 
 }
